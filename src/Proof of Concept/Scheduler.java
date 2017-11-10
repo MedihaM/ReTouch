21aa5b8da70acd839a27a61d67370e00d563326a
Abrar Attia <attiaa1@mcmaster.ca>
2017-11-09 20:14:01 -0500

    Scheduler comments

--- src/Proof of Concept/Scheduler.java
+++ src/Proof of Concept/Scheduler.java
@@ -7,32 +7,55 @@
 import java.util.Timer;
 import java.util.TimerTask;
 
+/**
+ * This class implements a timer
+ * @author Mediha Munimm
+ * @version 1.0
+ * @since 2017-10-16
+ */
 public class Scheduler extends TimerTask {
 
-	int elapsedTime = 0;
+	static int elapsedTime = 0;
 	static Timer timer = new Timer(true);
 	
 	@Override
+	/**
+	 * This method runs the completeTask method
+	 */
 	public void run() {
 		completeTask();
 	}
 
-	/*
-	 * Increases elapsed time by one every second.
+	/**
+	 * This method increases the elapsed time by one every second
 	 */
 	private void completeTask() {
 		elapsedTime++;
 		System.out.println(elapsedTime);
 	}
 	
+	/**
+	 * This method stops the timer
+	 */
 	public static void endTimer(){
 		timer.cancel();
 	}
 
+	/**
+	 * This method starts the timer
+	 */
 	public static void runTimer() {
 		TimerTask timerTask = new Scheduler();
 		timer.scheduleAtFixedRate(timerTask, 0, 1000);
 		System.out.println("Timer started");
+	}
+	
+	/**
+	 * This method returns the elapsed time
+	 * @return The elapsed time
+	 */
+	public static int getTime(){
+		return elapsedTime;
 	}
 
 }
